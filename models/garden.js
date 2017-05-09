c96cbc663a0bc5099bb79cf36517af7b8d5bb959
Ben Conner <ben.conner40@gmail.com>
2017-05-09 16:04:27 -0500

    bookshelf model talking to the gardens table

---
+++ models/garden.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const { bookshelf } = require('../db/database');
+
+require('./plant')
+require('./planted_garden')
+
+const Garden = bookshelf.Model.extend({
+  tableName: 'gardens',
+  plants: function() {return this.belongsToMany('Plant').through('Planted_Garden')}
+
+},{
+  getAll: function() {
+    return this.forge()
+    .fetchAll()
+    .then( (rows) => {
+      return rows
+    })
+    .catch( (error) => {
+      return error
+    });
+  },
+  getSingleGarden: function(id) {
+    // console.log("show id", id);
+    return this.forge({id})
+    .fetch()
+    .then( (garden) => {
+      return garden;
+    })
+    .catch( (error) => {
+      // console.log("error??", error);
+      return error;
+    });
+  }
+})
+
+module.exports = bookshelf.model('Garden', Garden);
