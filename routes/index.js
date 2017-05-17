86c169470df81bc02ec86af28d0098f31fe27c1c
Ben Conner <ben.conner40@gmail.com>
2017-05-17 10:52:43 -0500

    endpoints and modules requrired updated for work with pivot table

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,28 @@
 const { Router } = require('express')
 const router = Router()
 
-const { getGardens, getGardenById, addGarden, editGarden} = require('../controllers/garden')
+const { getGardens, getGardenById, addGarden, editGarden, getGardenAndPlants} = require('../controllers/garden')
 const {getPlants, getPlantById, addPlant, deletePlant, editPlant} = require('../controllers/plant')
-const {getGardenPlants, addGardenPlants, deleteGardenPlants, deleteGarden} = require('../controllers/planted_garden')
+const {getGardenPlants, addGardenPlants, deleteGardenPlants, deleteGarden, addPlantsToGarden} = require('../controllers/planted_garden')
 
 //garden routes
 router.get('/gardens', getGardens)
 router.get('/garden', getGardenById)
 // router.post('/gardens/new', addGarden)
-// router.delete('/gardens/delete/:id', deleteGarden)
 router.patch('/gardens/edit/:id', editGarden)
+// router.get('/gardens/all', getGardenAndPlants)
 
 //garden routes
 router.get('/plants', getPlants)
 router.get('/plants/:id', getPlantById)
 router.post('/plants/new', addPlant)
-// router.delete('/plants/delete/:id', deletePlant)
+router.delete('/plants/delete/:id', deletePlant)
 router.patch('/plants/edit/:id', editPlant)
 
 //relations
 router.post('/garden/plants', addGardenPlants)
-router.delete('/garden/plants', deleteGardenPlants)
+router.post('/plants/garden', addPlantsToGarden)
+router.delete('/garden/plants/:gardenId/:plantId', deleteGardenPlants)
 router.delete('/garden/delete', deleteGarden)
 
 module.exports = router
